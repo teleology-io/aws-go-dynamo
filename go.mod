1d1046dff692b94e19516443ddb4691284212892
Chris Sullivan <chrissullivan.dev@gmail.com>
2022-01-16 14:02:11 -0800

    Removing mapstructure mod

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/teleology-io/aws-go-dynamo
 
 go 1.17
 
-require (
-	github.com/aws/aws-sdk-go v1.42.4
-	github.com/mitchellh/mapstructure v1.4.3
-)
+require github.com/aws/aws-sdk-go v1.42.4
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- service_test.go
+++ service_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/mitchellh/mapstructure"
 )
 
 var TEST_INVITATION_ID = "test-invitation-id"
@@ -18,23 +17,6 @@ type Invitation struct {
 	Role      string `json:"role"`
 	Email     string `json:"email" dynamo:"gsi,EmailGSI"`
 	Expiry    int64  `json:"expiry"`
-}
-
-func Convert(in interface{}, out interface{}) error {
-	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		TagName: "json",
-		Result:  &out,
-	})
-	if err != nil {
-		return err
-	}
-
-	err = decoder.Decode(in)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 var sample = Invitation{
@@ -73,14 +55,7 @@ func init() {
 
 func TestGet(t *testing.T) {
 	// var out Invitation
-	out, err := table.Get(TEST_INVITATION_ID)
-	if err != nil {
-		t.Fail()
-	}
-
-	// check we can convert to our expected type
-	var invitation Invitation
-	err = Convert(out, &invitation)
+	_, err := table.Get(TEST_INVITATION_ID)
 	if err != nil {
 		t.Fail()
 	}
@@ -116,29 +91,20 @@ func TestCollisions(t *testing.T) {
 func TestCreateAndUpdate(t *testing.T) {
 	_, err := table.Create(sample.ID, sample)
 	if err != nil {
-		fmt.Println("creat er", err.Error())
 		t.Fail()
 	}
 
-	updateResult, err := table.Update(TEST_INVITATION_ID, Invitation{
+	_, err = table.Update(TEST_INVITATION_ID, Invitation{
 		ID:   TEST_INVITATION_ID,
 		Role: "publisher",
 	})
-	if err != nil {
-		fmt.Println("er", err.Error())
-		t.Fail()
-	}
-
-	// check we can convert to our expected type
-	invitation := Invitation{}
-	err = Convert(updateResult, &invitation)
 	if err != nil {
 		t.Fail()
 	}
 }
 
 func TestQuery(t *testing.T) {
-	out, err := table.Query([]QueryParams{
+	_, err := table.Query([]QueryParams{
 		{
 			Key:   "project_id",
 			Value: "085dea80-1e49-4cd1-a739-e9f355f08f2a",
@@ -150,13 +116,6 @@ func TestQuery(t *testing.T) {
 	})
 	if err != nil {
 		fmt.Println("query out", err.Error())
-		t.Fail()
-	}
-
-	invitations := make([]Invitation, len(out))
-	err = Convert(&out, &invitations)
-	if err != nil {
-		fmt.Println("query convert", err.Error())
 		t.Fail()
 	}
 }
